cc5389712764491bb236fe1296e52eeaea5ca3e0
Janne <janne.lahdentausta25@gmail.com>
2022-10-09 15:04:56 +0300

    Part 4 exercises

---
+++ Part4/BlogList/models/blog.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const uniqValid = require('mongoose-unique-validator')
+
+const blogSchema = mongoose.Schema({
+
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  likes: {
+    type: Number,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+  
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+blogSchema.plugin(uniqValid)
+
+
+module.exports = mongoose.model('Blog', blogSchema)
